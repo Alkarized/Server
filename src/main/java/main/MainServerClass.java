94c16b69a3ff75c1c65e64f840b6d976e5617407
Alkarized <golananime@gmail.com>
2021-04-05 00:58:42 +0300

    init server

--- src/main/java/main/MainServerClass.java
+++ src/main/java/main/MainServerClass.java
@@ -1,33 +1,51 @@
 package main;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainServerClass {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ServerSocket serverSocket = new ServerSocket(1707);
-        Socket socket = serverSocket.accept();
-        System.out.println("Запрос принят - " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
-        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-        ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-        TestClass testClass = (TestClass) input.readObject();
-        System.out.println("Полученное значение - " + testClass.x);
-        testClass.x = 12;
-        output.writeObject(testClass);
-        output.flush();
-        System.out.println("Отправлено");
-        /*socket.close();
-        serverSocket.close();*/
-    }
-}
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(1707));
+        serverSocketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-class TestClass implements Serializable {
-    private static final long serialVersionUID = 1012312321;
-    int x = 5;
+        while (true){
+            selector.select();
 
-    public void say(){
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectedKeys.iterator();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            while (iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
 
+                if (selectionKey.isAcceptable()){
+                    SocketChannel client = serverSocketChannel.accept();
+                    System.out.println("Есть клиент - " + client.getRemoteAddress());
+                    client.configureBlocking(false);
+                    client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+
+                }
+
+                if (selectionKey.isReadable() && selectionKey.isWritable()){
+                    SocketChannel client = (SocketChannel) selectionKey.channel();
+                    client.read(byteBuffer);
+                    byteBuffer.rewind();
+                    System.out.println(Arrays.toString(byteBuffer.array()));
+                    byteBuffer.clear();
+                }
+                iterator.remove();
+            }
+        }
     }
 }
 
