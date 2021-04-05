c544fd2b4b5927cf6c13ad1fe78662df1dabe0ca
Alkarized <golananime@gmail.com>
2021-04-05 13:34:35 +0300

    make workable Server

--- src/main/java/main/MainServerClass.java
+++ src/main/java/main/MainServerClass.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -12,8 +14,14 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class MainServerClass {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serverSocket = new ServerSocket(1707);
+        Socket socket = serverSocket.accept();
+        ObjectInputStream object = new ObjectInputStream(socket.getInputStream());
+        System.out.println(object.readObject());
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(new Serial());
+        objectOutputStream.flush();
     }
 }
 
--- src/main/java/main/Serial.java
+++ src/main/java/main/Serial.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.Serializable;
 
 public class Serial implements Serializable {
-    private static final long serialVersionUID = 23112;
     private File file;
 
     public Serial(File file) {
