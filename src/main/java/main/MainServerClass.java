7cbb7ed63d0b831b8c2d5735a09b640d253a8e5e
Alkarized <golananime@gmail.com>
2021-04-13 02:06:01 +0300

    something is going well, believe we're on the right way

--- src/main/java/main/MainServerClass.java
+++ src/main/java/main/MainServerClass.java
@@ -8,8 +8,15 @@ import utils.CSVFileReader;
 import utils.ProgramStarter;
 
 public class MainServerClass {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        read2();
+
+    public static void main(String[] args) {
+
+        String[] test = new String[1];
+        test[0] = "test";
+
+        //ProgramStarter programStarter = new ProgramStarter("localhost",1707, args);
+        ProgramStarter programStarter = new ProgramStarter("localhost",1707, test);
+        programStarter.startProgram();
     }
 
 }
